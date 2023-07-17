8d32c5a77c3eb570e41394438f547e86820eb7ff
Greg Gannicott <greg@gannicott.co.uk>
2023-07-17 16:51:04 +0100

    Gitsigns: Various
    - Rework mappings to follow new conventions.
    - Add mapping to stage a hunk

--- nvim/.config/nvim/lua/core/plugin-config/gitsigns.lua
+++ nvim/.config/nvim/lua/core/plugin-config/gitsigns.lua
@@ -9,9 +9,10 @@ require('gitsigns').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').prev_hunk,
+          { buffer = bufnr, desc = '[P]revious [H]unk' })
+        vim.keymap.set('n', '<leader>nh', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[N]ext [H]unk' })
+        vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview [G]it [H]unk' })
+        vim.keymap.set('n', '<leader>gsh', require('gitsigns').stage_hunk, { buffer = bufnr, desc = 'Stage Git Hunk' })
       end,
 })
