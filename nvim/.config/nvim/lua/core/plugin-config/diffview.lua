f74cca69d7bbd8eadb38a472856a5041c74fc168
Greg Gannicott <greg@gannicott.co.uk>
2023-07-18 17:31:48 +0100

    Neovim: Add description to mapping

--- nvim/.config/nvim/lua/core/plugin-config/diffview.lua
+++ nvim/.config/nvim/lua/core/plugin-config/diffview.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<leader>gdb', ':DiffviewOpen origin/main... --imply-local<CR>')
+vim.keymap.set('n', '<leader>gdb', ':DiffviewOpen origin/main... --imply-local<CR>', { desc = 'Compare current branch with main.'})
