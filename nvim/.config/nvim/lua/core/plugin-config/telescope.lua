f8c856f3dbfca731e346d74a01c476f5a2068048
Greg Gannicott <greg@gannicott.co.uk>
2023-07-18 22:28:03 +0100

    Telescope: Update mappings so which-key is used to define them

--- nvim/.config/nvim/lua/core/plugin-config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugin-config/telescope.lua
@@ -1,3 +1,4 @@
+local wk = require("which-key")
 local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 telescope.setup {
@@ -13,15 +14,20 @@ telescope.setup {
 pcall(telescope.load_extension, 'fzf')
 
 -- Define keymappings to display presets
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch existing [B]uffers' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch git [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').lsp_references, { desc = '[S]earch [R]eferences' })
-vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find,
-  { desc = '[/] Fuzzily search in current buffer' })
+wk.register({
+  ["?"] = {require('telescope.builtin').oldfiles, '[?] Find recently opened files' },
+  ["/"] = {require('telescope.builtin').current_buffer_fuzzy_find, '[/] Fuzzily search in current buffer' },
+  s = {
+    name = "Search",
+    b = {require('telescope.builtin').buffers,'[S]earch existing [B]uffers' },
+    d = {require('telescope.builtin').diagnostics, '[S]earch [D]iagnostics' },
+    f = {require('telescope.builtin').find_files, "[S]earch git [F]iles" },
+    g = {require('telescope.builtin').live_grep, '[S]earch by [G]rep' },
+    h = {require('telescope.builtin').help_tags, '[S]earch [H]elp' },
+    r = {require('telescope.builtin').lsp_references, '[S]earch [R]eferences' },
+  },
+}, { prefix = "<leader>" })
+
 vim.keymap.set('n', '<C-p>', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').git_files(require('telescope.themes').get_dropdown {
@@ -33,4 +39,3 @@ vim.keymap.set('n', '<C-p>', function()
     }
   })
 end, { desc = '[p] Find files' })
-
