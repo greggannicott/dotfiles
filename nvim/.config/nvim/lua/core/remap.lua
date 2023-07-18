449401e78999eddedc0f915b24f1b8d6a56dbebf
Greg Gannicott <greg@gannicott.co.uk>
2023-07-18 21:12:51 +0100

    Add mapping to open config file

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -87,3 +87,6 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+
+-- Mapping for opening config file
+vim.keymap.set('n', '<leader>.', ':vs ~/.config/nvim/init.lua<CR>', { desc='Open Neovim Config'})
