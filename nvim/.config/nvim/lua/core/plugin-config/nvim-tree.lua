c1736cd309a74d940059b65aa76c9dd9c327bf58
Greg Gannicott <greg@gannicott.co.uk>
2023-07-19 22:38:09 +0100

    Neovim - nvim-tree
    * Update mappings to use which-key.register()
    * Add mapping so pressing `l` when highlighting a folder or file opens
      it
    * Add mapping to select the currently open file (eg. focus it).
    * Add mapping to expand the window to a set size.

--- nvim/.config/nvim/lua/core/plugin-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugin-config/nvim-tree.lua
@@ -1,5 +1,32 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
-vim.keymap.set('n', '<leader>ee', ':NvimTreeToggle<CR>')
+local wk = require('which-key')
+
+-- Define key mappings to be used within the plugin
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+end
+
+
+require('nvim-tree').setup({
+  on_attach = my_on_attach
+})
+
+-- Mappings
+wk.register({
+  e = {
+    name = 'Explorer',
+    e = { ':NvimTreeToggle<CR>', 'Toggle' },
+    f = { ':NvimTreeFindFile<CR>', 'Focus Current File' },
+    x = { ':NvimTreeResize 70<CR>', 'Expand' },
+  }
+}, { prefix = '<leader>'})
