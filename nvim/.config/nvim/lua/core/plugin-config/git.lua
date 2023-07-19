8eb4f137176dc39afad8834679cbad83e79c547b
Greg Gannicott <greg@gannicott.co.uk>
2023-07-19 21:14:24 +0100

    Neovim: Merge git related plugins into one file

--- nvim/.config/nvim/lua/core/plugin-config/git.lua
+++ nvim/.config/nvim/lua/core/plugin-config/git.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_set_keymap('n', '<Leader>gg', ':Git<CR>', { desc = '[G]it' } )
+vim.api.nvim_set_keymap('n', '<Leader>gc', ':Git commit<CR>', { desc = '[G]it [C]ommit' } ) 
+vim.api.nvim_set_keymap('n', '<Leader>ga', ':Git commit --amend<CR>', { desc = '[G]it [A]mend Commit' } ) 
+
 require('gitsigns').setup({
 
       -- See `:help gitsigns.txt`
