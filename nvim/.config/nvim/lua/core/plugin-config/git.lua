8bc7fe41e0b1774d2329c2348b91d03ace44c492
Greg Gannicott <greg@gannicott.co.uk>
2023-07-26 22:29:26 +0100

    Neovim - Git
    * New mapping: 'Toggle Line Blame'
    * Formatting changes as a result of autoformatting being in place

--- nvim/.config/nvim/lua/core/plugin-config/git.lua
+++ nvim/.config/nvim/lua/core/plugin-config/git.lua
@@ -2,24 +2,24 @@ local wk = require('which-key')
 
 require('gitsigns').setup({
 
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        -- Note: the 'which' description for these mappings are handled below as you were
-        -- unable to get which-key.register to work within the on_attach.
-        vim.keymap.set('n', '<leader>ghp', require('gitsigns').prev_hunk,
-          { buffer = bufnr })
-        vim.keymap.set('n', '<leader>ghn', require('gitsigns').next_hunk, { buffer = bufnr})
-        vim.keymap.set('n', '<leader>ghv', require('gitsigns').preview_hunk, { buffer = bufnr})
-        vim.keymap.set('n', '<leader>ghs', require('gitsigns').stage_hunk, { buffer = bufnr})
-        vim.keymap.set('n', '<leader>ghr', require('gitsigns').reset_hunk, { buffer = bufnr})
-      end,
+  -- See `:help gitsigns.txt`
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  on_attach = function(bufnr)
+    -- Note: the 'which' description for these mappings are handled below as you were
+    -- unable to get which-key.register to work within the on_attach.
+    vim.keymap.set('n', '<leader>ghp', require('gitsigns').prev_hunk,
+      { buffer = bufnr })
+    vim.keymap.set('n', '<leader>ghn', require('gitsigns').next_hunk, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>ghv', require('gitsigns').preview_hunk, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>ghs', require('gitsigns').stage_hunk, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>ghr', require('gitsigns').reset_hunk, { buffer = bufnr })
+  end,
 })
 
 wk.register({
@@ -32,11 +32,12 @@ wk.register({
   ['<leader>'] = {
     g = {
       name = "Git",
-      g = {':Git<CR>', 'Git Status'},
+      b = { ':Gitsigns toggle_current_line_blame<CR>', 'Toggle Line Blame' },
+      g = { ':Git<CR>', 'Git Status' },
       c = {
         name = "Commit",
-        c = {':Git commit<CR>', 'Git Commit'},
-        a = {':Git commit --amend<CR>', 'Git Commit Amend'},
+        c = { ':Git commit<CR>', 'Git Commit' },
+        a = { ':Git commit --amend<CR>', 'Git Commit Amend' },
       },
       h = {
         name = "Hunk",
@@ -47,8 +48,8 @@ wk.register({
         r = "Reset Hunk"
       },
       d = {
-          name = "Diff",
-          b = {':DiffviewOpen origin/main... --imply-local<CR>', "Diff branch with main"}
+        name = "Diff",
+        b = { ':DiffviewOpen origin/main... --imply-local<CR>', "Diff branch with main" }
       }
     },
   }
