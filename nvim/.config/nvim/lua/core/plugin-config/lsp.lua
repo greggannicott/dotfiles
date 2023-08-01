4afb6224ce03035a2e7e1494ee56d5cb7a3dbb10
Greg Gannicott <greg@gannicott.co.uk>
2023-08-01 15:12:35 +0100

    Neovim - LSP
    * Rearraneg the mappings
    * Change 'Goto Implementation' to use Telescope.

--- nvim/.config/nvim/lua/core/plugin-config/lsp.lua
+++ nvim/.config/nvim/lua/core/plugin-config/lsp.lua
@@ -16,25 +16,34 @@ local on_attach = function(_, bufnr)
   nmap('<leader>ca', vim.lsp.buf.code_action, 'Code Actions')
 
   nmap('gd', vim.lsp.buf.definition, 'Goto Definition')
+  nmap('<leader>cd', vim.lsp.buf.definition, 'Goto Definition')
+
   nmap('gr', require('telescope.builtin').lsp_references, 'Goto References')
-  nmap('gI', vim.lsp.buf.implementation, 'Goto Implementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type Definition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+  nmap('<leader>cr', require('telescope.builtin').lsp_references, 'Search References')
+
+  nmap('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
+  nmap('<leader>cI', require('telescope.builtin').lsp_implementations, 'Search Implementations')
+
+  nmap('<leader>cD', vim.lsp.buf.type_definition, 'Goto Type Definition')
+
+  nmap('<leader>cy', require('telescope.builtin').lsp_document_symbols, 'Search Document Symbols')
+  nmap('<leader>cY', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Search Workspace Symbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<leader>cK', vim.lsp.buf.hover, 'Hover Documentation')
   -- Commented out as it was clashing with something else. Remap it when you get around
   -- to having a standard set of mappings for LSP stuff.
   -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, 'Goto Declaration')
-  nmap('<leader>d[', vim.diagnostic.goto_prev, 'Go to previous error')
+  nmap('<leader>ce', vim.lsp.buf.declaration, 'Goto Declaration')
+  nmap('<leader>c[', vim.diagnostic.goto_prev, 'Go to previous error')
   nmap('[e', vim.diagnostic.goto_prev, 'Go to previous error')
-  nmap('<leader>d]', vim.diagnostic.goto_next, 'Go to next error')
+  nmap('<leader>c]', vim.diagnostic.goto_next, 'Go to next error')
   nmap(']e', vim.diagnostic.goto_next, 'Go to next error')
 
+  -- Give the '<leader>c' the name 'Code' in which-key.
   wc.register({
     c = {
       name = 'Code'
