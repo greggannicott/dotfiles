53113e74e6990d660f6fdd0a2ded7b6a97d17d44
Greg Gannicott <greg@gannicott.co.uk>
2023-08-01 21:42:23 +0100

    Neovim - telescope
    * Add search for Tagstack

--- nvim/.config/nvim/lua/core/plugin-config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugin-config/telescope.lua
@@ -39,6 +39,7 @@ wk.register({
     m = {require('telescope.builtin').marks, 'Search Marks' },
     r = {require('telescope.builtin').lsp_references, 'Search References' },
     s = {require('telescope.builtin').grep_string, 'Grep String Under Cursor' },
+    t = {require('telescope.builtin').tagstack, 'Search Tagstack' },
     Y = {require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Search Workspace Symbols' },
     y = {require('telescope.builtin').lsp_document_symbols, 'Search Document Symbols' },
   },
