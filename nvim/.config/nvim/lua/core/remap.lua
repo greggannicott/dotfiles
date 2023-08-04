258b094db3b48c7c062f6cb0bfd543b6473a3f7d
Greg Gannicott <greg@gannicott.co.uk>
2023-08-04 18:56:52 +0100

    Neovim - General
    * Set tabstop to 4. 8 tab stops (spaces representing a tab) is ridiculous.

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,6 +1,6 @@
-local wk = require('which-key')
+local wk = require("which-key")
 vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 
 -- Set highlight on search
 vim.o.hlsearch = true
@@ -9,12 +9,12 @@ vim.o.hlsearch = true
 vim.wo.number = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -27,7 +27,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -35,45 +35,50 @@ vim.o.timeout = true
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect,noinsert,preview'
+vim.o.completeopt = "menuone,noselect,noinsert,preview"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
+-- Formatting.
+
 -- Don't start new lines with comment symbols if current line has comment symbols
-vim.o.formatoptions=cro
+vim.o.formatoptions = cro
+
+-- Set tab width. Default appears to be 8!
+vim.o.tabstop = 4
 
 -- Set relative numbers
 vim.wo.relativenumber = true
 
 -- Display the cursor line
 vim.wo.cursorline = true
-vim.wo.cursorlineopt = 'both'
+vim.wo.cursorlineopt = "both"
 
 -- Don't display status information under each window
 vim.go.laststatus = 3
 
 -- Display the name and state of each file in the top right
-vim.go.winbar = '%=%m %t'
+vim.go.winbar = "%=%m %t"
 
 -- Open file to right when performing split
 vim.go.splitright = true
@@ -84,25 +89,25 @@ vim.o.scrolloff = 8
 
 -- Tab related mappings
 wk.register({
-  T = {
-    name = 'Tabs',
-    c = { '<cmd>:tabclose<CR>', 'Close Tab' },
-    h = { '<cmd>:tabprevious<CR>', 'Previous Tab' },
-    l = { '<cmd>:tabnext<CR>', 'Next Tab' },
-    n = { '<cmd>:tabnew<CR>', 'New Tab' }
-  }
-}, { prefix = '<leader>' })
-vim.keymap.set('n', 'L', '<cmd>tabnext<CR>', { desc = 'Next Tab' })
-vim.keymap.set('n', 'H', '<cmd>tabprevious<CR>', { desc = 'Previous Tab' })
+	T = {
+		name = "Tabs",
+		c = { "<cmd>:tabclose<CR>", "Close Tab" },
+		h = { "<cmd>:tabprevious<CR>", "Previous Tab" },
+		l = { "<cmd>:tabnext<CR>", "Next Tab" },
+		n = { "<cmd>:tabnew<CR>", "New Tab" },
+	},
+}, { prefix = "<leader>" })
+vim.keymap.set("n", "L", "<cmd>tabnext<CR>", { desc = "Next Tab" })
+vim.keymap.set("n", "H", "<cmd>tabprevious<CR>", { desc = "Previous Tab" })
 
 -- Remap navigation of windows so you can just use ctrl + h/j/k/l
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
 
 -- Mapping for opening config file
-vim.keymap.set('n', '<leader>.', ':vs ~/.config/nvim/init.lua<CR>', { desc='Open Neovim Config'})
+vim.keymap.set("n", "<leader>.", ":vs ~/.config/nvim/init.lua<CR>", { desc = "Open Neovim Config" })
 
 -- Plugin Development mappings
-vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>', { desc='Source current file'})
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Source current file" })
