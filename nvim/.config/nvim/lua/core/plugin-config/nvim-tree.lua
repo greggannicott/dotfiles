e4c3deb9b4f23bfe5038cc60c8f58fbf83677be7
Greg Gannicott <greg@gannicott.co.uk>
2023-08-04 23:07:06 +0100

    Neovim - nvim-tree
    * Make git changes easier to see.
    * Formatting changes.

--- nvim/.config/nvim/lua/core/plugin-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugin-config/nvim-tree.lua
@@ -1,34 +1,36 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-local wk = require('which-key')
+local wk = require("which-key")
 
 -- Define key mappings to be used within the plugin
 local function my_on_attach(bufnr)
-  local api = require('nvim-tree.api')
+	local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
 
-  api.config.mappings.default_on_attach(bufnr)
+	api.config.mappings.default_on_attach(bufnr)
 
-  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
 end
 
-
-require('nvim-tree').setup({
-  on_attach = my_on_attach
+require("nvim-tree").setup({
+	on_attach = my_on_attach,
+	renderer = {
+		highlight_git = true,
+	},
 })
 
 -- Mappings
 wk.register({
-  e = {
-    name = 'Explorer',
-    c = { ':NvimTreeCollapse<CR>', 'Collapse All Nodes' },
-    e = { ':NvimTreeToggle<CR>', 'Toggle' },
-    f = { ':NvimTreeFindFile<CR>', 'Focus Current File' },
-    r = { ':NvimTreeResize 30<CR>', 'Reduce Window Size' },
-    x = { ':NvimTreeResize 70<CR>', 'Expand Window Size' },
-  }
-}, { prefix = '<leader>'})
+	e = {
+		name = "Explorer",
+		c = { ":NvimTreeCollapse<CR>", "Collapse All Nodes" },
+		e = { ":NvimTreeToggle<CR>", "Toggle" },
+		f = { ":NvimTreeFindFile<CR>", "Focus Current File" },
+		r = { ":NvimTreeResize 30<CR>", "Reduce Window Size" },
+		x = { ":NvimTreeResize 70<CR>", "Expand Window Size" },
+	},
+}, { prefix = "<leader>" })
