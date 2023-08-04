c0259e788ca1b1e335cd20e02972d07ca14b9be4
Greg Gannicott <greg@gannicott.co.uk>
2023-08-04 18:55:49 +0100

    Neovim: null-ls
    * Re-introduce formatting for lua. Maybe this time it won't break HTML
      formatting.

--- nvim/.config/nvim/lua/core/plugin-config/prettier.lua
+++ nvim/.config/nvim/lua/core/plugin-config/prettier.lua
@@ -6,49 +6,48 @@ local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
 
 null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.keymap.set("n", "<Leader>f", function()
+				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+			end, { buffer = bufnr, desc = "[lsp] format" })
 
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
+			-- format on save
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+			vim.api.nvim_create_autocmd(event, {
+				buffer = bufnr,
+				group = group,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr, async = async })
+				end,
+				desc = "[lsp] format on save",
+			})
+		end
 
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
-  -- Add auto-formatting for Lua
-  -- NOTE: Commented out as it was preventing Prettier from formatting html files.
-  -- sources = {
-  --   null_ls.builtins.formatting.stylua
-  -- }
+		if client.supports_method("textDocument/rangeFormatting") then
+			vim.keymap.set("x", "<Leader>f", function()
+				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+			end, { buffer = bufnr, desc = "[lsp] format" })
+		end
+	end,
+	-- Add auto-formatting for Lua
+	sources = {
+		null_ls.builtins.formatting.stylua,
+	},
 })
 
 local prettier = require("prettier")
 
 prettier.setup({
-  bin = 'prettierd',
-  filetypes = {
-    "css",
-    "html",
-    "javascript",
-    "json",
-    "markdown",
-    "scss",
-    "typescript",
-    "yaml",
-  },
+	bin = "prettierd",
+	filetypes = {
+		"css",
+		"html",
+		"javascript",
+		"json",
+		"markdown",
+		"scss",
+		"typescript",
+		"yaml",
+	},
 })
