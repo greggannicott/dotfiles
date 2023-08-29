5b6c9257f636c5be8fafa2c80a0d461c82349732
Greg Gannicott <greg@gannicott.co.uk>
2023-08-29 10:38:33 +0100

    Neovim - Provide Harpoon with a name in which-key

--- nvim/.config/nvim/lua/core/plugin-config/harpoon.lua
+++ nvim/.config/nvim/lua/core/plugin-config/harpoon.lua
@@ -2,6 +2,7 @@ local wk = require("which-key")
 
 wk.register({
 	h = {
+		name = "Harpoon",
 		a = { require("harpoon.mark").add_file, "Add File" },
 		h = { require("harpoon.ui").toggle_quick_menu, "Display Marks" },
 	},
