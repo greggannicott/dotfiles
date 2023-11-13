98a67a5f020901789d0b30b9ab522e2469a93407
Greg Gannicott <greg@gannicott.co.uk>
2023-11-13 12:17:03 +0000

    ts.zsh - Formatting changes

--- bin/bin/ts.zsh
+++ bin/bin/ts.zsh
@@ -2,10 +2,10 @@ repos=$(find ~/code -d 1 -type d -print | sed 's/ /SPACE/g')
 
 for repo in $repos
 do
-		unescapedRepo=$(echo $repo | sed 's/SPACE/ /g')
-		if [ -d "$unescapedRepo/.git/" ]; then
-				git_repos+="${repo}\n"
-		fi
+    unescapedRepo=$(echo $repo | sed 's/SPACE/ /g')
+    if [ -d "$unescapedRepo/.git/" ]; then
+        git_repos+="${repo}\n"
+    fi
 done
 selected_repo=$(printf $git_repos | sed 's/SPACE/ /g' | fzf-tmux)
 # selected=$(printf "$repos\n" | fzf)
