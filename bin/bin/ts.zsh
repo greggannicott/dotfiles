2e6879d0c9a1172026ca1b5d5ee13e6c966d3cdb
Greg Gannicott <greg@gannicott.co.uk>
2023-11-13 17:27:20 +0000

    Create or attach to session.
    If session already exists, attach to it.
    
    If no session exists, create it.
    
    Note that this only works when running from outside of tmux.

--- bin/bin/ts.zsh
+++ bin/bin/ts.zsh
@@ -1,3 +1,4 @@
+# Find all git repos in ~/code
 repos=$(find ~/code -d 1 -type d -prune -print | sed 's/ /SPACE/g')
 
 for repo in $repos
@@ -7,5 +8,16 @@ do
         git_repos+="${repo}\n"
     fi
 done
-selected_repo=$(printf $git_repos | sed 's/SPACE/ /g' | fzf-tmux)
-echo $selected_repo
+selected_repo_path=$(printf $git_repos | sed 's/SPACE/ /g' | fzf-tmux)
+
+
+# Find out whether a session already exists for this repos
+folder_name=$(basename $selected_repo_path)
+existing_session=$(tmux list-sessions |  cut -d ':' -f 1 | grep $folder_name )
+
+# If no session exists, create one. Otherwise, attach to the existing session
+if [ -z $existing_session ]; then
+    tmux new-session -s $folder_name -c $selected_repo_path
+else
+    tmux attach-session -t $existing_session
+fi
