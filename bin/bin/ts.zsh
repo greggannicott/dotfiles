735dbf91306b8dd8d460c9a20f26198777cd4d94
Greg Gannicott <greg@gannicott.co.uk>
2023-11-14 10:42:15 +0000

    Account for whether the user is alreawdy inside of tmux when creating/joining session.

--- bin/bin/ts.zsh
+++ bin/bin/ts.zsh
@@ -1,3 +1,5 @@
+# #!/bin/zsh
+
 # Find all git repos in ~/code
 repos=$(find ~/code -d 1 -type d -prune -print | sed 's/ /SPACE/g')
 
@@ -15,9 +17,23 @@ selected_repo_path=$(printf $git_repos | sed 's/SPACE/ /g' | fzf-tmux)
 folder_name=$(basename $selected_repo_path)
 existing_session=$(tmux list-sessions |  cut -d ':' -f 1 | grep $folder_name )
 
-# If no session exists, create one. Otherwise, attach to the existing session
+# If no session exists, create one
 if [ -z $existing_session ]; then
-    tmux new-session -s $folder_name -c $selected_repo_path
+    # If we're not in tmux, create a new session and attach to it
+    if [ -z $TMUX ]; then
+        tmux new-session -s $folder_name -c $selected_repo_path
+        # If we're in tmux, create a new detached session and switch to it
+    else
+        tmux new-session -d -s $folder_name -c $selected_repo_path
+        tmux switch-client -t $folder_name
+    fi
+    # If a session exists, attach to it
 else
-    tmux attach-session -t $existing_session
+    # If we're not in tmux, attach to the session
+    if [ -z $TMUX ]; then
+        tmux attach-session -t $existing_session
+        # If we're in tmux, switch to the session
+    else
+        tmux switch-client -t $existing_session
+    fi
 fi
