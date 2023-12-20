3e1e7c65749741b8e50f710f6e01a9c3b705c597
Greg Gannicott <greg.gannicott@precisely.com>
2023-12-20 12:18:13 +0000

    TASK-1825 - nvim - Fix mapping clash when using autocomplete
    https://www.notion.so/gregggannicott/Fix-mapping-clash-when-navigating-through-snippet-placeholders-and-selecting-a-completion-7e190077a676403a8654c331f0225962?pvs=4

--- nvim/.config/nvim/lua/core/plugin-config/completions.lua
+++ nvim/.config/nvim/lua/core/plugin-config/completions.lua
@@ -23,7 +23,12 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-n>"] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
 			else
 				fallback()
@@ -32,7 +37,12 @@ cmp.setup({
 		["<C-k>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-p>"] = cmp.mapping(function(fallback)
+			if luasnip.locally_jumpable(-1) then
 				luasnip.jump(-1)
 			else
 				fallback()
