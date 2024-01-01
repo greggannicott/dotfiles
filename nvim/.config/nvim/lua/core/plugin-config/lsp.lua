de236d8b4d3bd349e359e1573365cac931773b9f
Greg Gannicott <greg@gannicott.co.uk>
2024-01-01 17:24:37 +0000

    nvim - Prevent tsserver from applying formatting.
    This is to try and stop a class with Prettier.

--- nvim/.config/nvim/lua/core/plugin-config/lsp.lua
+++ nvim/.config/nvim/lua/core/plugin-config/lsp.lua
@@ -1,7 +1,11 @@
 local wc = require("which-key")
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
+	-- Prevent "tsserver" from formatting code. This is to prevent it from clashing with Prettier.
+	if client.name == "tsserver" then
+		client.server_capabilities.documentFormattingProvider = false
+	end
 	-- NOTE: Remember that lua is a real programming language, and as such it is possible
 	-- to define small helper and utility functions so you don't have to repeat yourself
 	-- many times.
@@ -141,7 +145,7 @@ require("glance").setup({
 		position = "right", -- Position of the list window 'left'|'right'
 		width = 0.33, -- 33% width relative to the active window, min 0.1, max 0.5
 	},
-	theme = {         -- This feature might not work properly in nvim-0.7.2
+	theme = { -- This feature might not work properly in nvim-0.7.2
 		enable = true, -- Will generate colors for the plugin based on your current colorscheme
 		mode = "auto", -- 'brighten'|'darken'|'auto', 'auto' will set mode based on the brightness of your colorscheme
 	},
