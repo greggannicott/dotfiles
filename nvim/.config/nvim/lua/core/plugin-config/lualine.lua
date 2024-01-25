f5d55b393d93948bac89f7a62a5d42a301d957bc
Greg Gannicott <greg@gannicott.co.uk>
2024-01-25 22:14:51 +0000

    nvim - Fix issue with Noice that prevents macros from working

--- nvim/.config/nvim/lua/core/plugin-config/lualine.lua
+++ nvim/.config/nvim/lua/core/plugin-config/lualine.lua
@@ -1,8 +1,18 @@
-require('lualine').setup({
+require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = 'onedark',
-		component_separators = '|',
-		section_separators = '',
-	}
+		theme = "onedark",
+		component_separators = "|",
+		section_separators = "",
+	},
+	sections = {
+		-- Required for macros to work. Without this pressing Q doesn't do anything.
+		lualine_x = {
+			{
+				require("noice").api.statusline.mode.get,
+				cond = require("noice").api.statusline.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+		},
+	},
 })
