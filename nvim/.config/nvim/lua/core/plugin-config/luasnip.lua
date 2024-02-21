6bc820c270bd1cc346e2f10854948d5a29a25db4
Greg Gannicott <greg@gannicott.co.uk>
2024-02-21 21:00:24 +0000

    nvim - Add angular snippets

--- nvim/.config/nvim/lua/core/plugin-config/luasnip.lua
+++ nvim/.config/nvim/lua/core/plugin-config/luasnip.lua
@@ -1,13 +1,15 @@
-local ls = require 'luasnip'
+local ls = require("luasnip")
 -- config
 
-ls.config.set_config {
-  history = true,
-  updateevents = "TextChanged,TextChangedI"
-}
+ls.config.set_config({
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+})
 
 -- Shortcut to source your Snippets
 vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/core/plugin-config/luasnip.lua<CR>")
 
 -- Import Snippets
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/core/snippets"})
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/core/snippets" })
+require("luasnip").filetype_extend("typescript", { "angular" })
+require("luasnip").filetype_extend("html", { "angular" })
