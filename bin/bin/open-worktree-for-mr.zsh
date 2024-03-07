fde9e23b804c81c9fc21ed00518424e49b39659d
Greg Gannicott <greg.gannicott@precisely.com>
2024-03-07 12:30:25 +0000

    open-worktree-for-mr: Introducing...

---
+++ bin/bin/open-worktree-for-mr.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+merge_requests=''
+
+# Generate list of merge requests to choose from
+glab mr list | sed '1,2d;$d'| while IFS= read -r line
+do
+    name=`echo $line | awk -F '\t' '{print $3}'`
+    branches=`echo $line | awk -F '\t' '{print $4}'`
+    merge_requests+="'$name' -- $branches\n"
+done
+
+# Obtain selection from user
+selected_merge_request=` echo $merge_requests | fzf `
+
+# Extract branch from selection
+source_branch=`echo $selected_merge_request | awk -F ' -- ' '{print $2}' | awk -F ' ← ' '{print $2}' | sed 's/^(//' | sed 's/)$//'`
+
+# See if worktree already exists
+existing_worktree_count=`git worktree list | awk '{ print $3 }' | sed 's/^\[//' | sed 's/\]$//' | grep --count $source_branch`
+
+# If worktree exists, open tmux session
+if [[ $existing_worktree_count -eq 1 ]]
+then
+    echo "Worktree for branch '$source_branch' already exists. Opening tmux session for you..."
+    tmux-too-young open --search $source_branch
+else
+    echo "Worktree for branch '$source_branch' does not exist."
+    echo
+    echo "At present this script does not support creating the worktree."
+fi
+
+echo
+
