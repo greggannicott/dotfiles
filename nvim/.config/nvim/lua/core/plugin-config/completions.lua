5fa3587273cda538cb0b076535d319915cc9ec2c
Greg Gannicott <greg.gannicott@precisely.com>
2024-06-24 16:54:29 +0100

    nvim - Stop c-n and c-p from navigating text suggestion lists.
    You were finding you'd use them rather than c-j and c-k and as a result
    you'd lose the LSP based suggestions.

--- nvim/.config/nvim/lua/core/plugin-config/completions.lua
+++ nvim/.config/nvim/lua/core/plugin-config/completions.lua
@@ -27,23 +27,9 @@ cmp.setup({
 				fallback()
 			end
 		end, { "i", "s" }),
-		["<C-n>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
 		["<C-k>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<C-p>"] = cmp.mapping(function(fallback)
-			if luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
 			else
 				fallback()
 			end
