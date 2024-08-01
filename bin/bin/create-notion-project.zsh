a85bdf1b32c611ac0bd53a6e2f44a41b4feca2b2
Greg Gannicott <greg.gannicott@precisely.com>
2024-08-01 17:31:07 +0100

    create-notion-project: Ability to specify type

--- bin/bin/create-notion-project.zsh
+++ bin/bin/create-notion-project.zsh
@@ -46,6 +46,16 @@ for (( i = 1; i <= $#; i++ )); do
                 exit 1
             fi
             ;;
+        --type)
+            # Check if the next argument exists
+            if [[ $((i + 1)) -le $# ]]; then
+                project_type_argument="${(P)$((i + 1))}"
+                ((i++))  # Skip the next argument since it's the value for --branch
+            else
+                echo "Error: --branch requires a value."
+                exit 1
+            fi
+            ;;
         --help)
             help=true
             ;;
@@ -63,8 +73,37 @@ if [[ $help == true ]]; then
     echo "  --jira-id <id>      The Jira ID of the project."
     echo "  --branch <branch>   The branch name of the project."
     echo "  --current           Set the project as the current project."
+    echo "  --type <type>       The type of the project. Valid values are: story, bug, general project, general area, code review, training subject."
     echo "  --help              Display this help message."
     exit 0
+fi
+
+# If project type argument has been set, map it to the correct value
+if [ -n "${project_type_argument}" ]; then
+    case "${project_type_argument}" in
+        story)
+            project_type='Story'
+            ;;
+        bug)
+            project_type='Bug'
+            ;;
+        "general project")
+            project_type='General Project'
+            ;;
+        "general area")
+            project_type='General Area'
+            ;;
+        "code review")
+            project_type='Code Review'
+            ;;
+        "training subject")
+            project_type='Training Subject'
+            ;;
+        *)
+            echo "Error: Invalid value for --type. Valid values are: story, bug, general project, general area, code review, training subject."
+            exit 1
+            ;;
+    esac
 fi
 
 data='
