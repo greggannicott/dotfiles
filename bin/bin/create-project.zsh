dd6b98ed8845d0e7d4753a389f2d666a48c74155
Greg Gannicott <greg.gannicott@precisely.com>
2024-08-02 12:15:16 +0100

    create-project - Ability to create Notion project

--- bin/bin/create-project.zsh
+++ bin/bin/create-project.zsh
@@ -37,13 +37,36 @@ create_worktree ()
 ui=false
 bff=false
 shell=false
-copy_branch=false
+name=""
+project_type="story"
 branch_name=""
+skip_notion=false
+copy_branch=false
 help=false
 
 # Parse arguments
 for (( i = 1; i <= $#; i++ )); do
     case "${(P)i}" in
+        --name)
+            # Check if the next argument exists
+            if [[ $((i + 1)) -le $# ]]; then
+                name="${(P)$((i + 1))}"
+                ((i++))  # Skip the next argument since it's the value for --branch
+            else
+                echo "Error: --name requires a value."
+                exit 1
+            fi
+            ;;
+        --type)
+            # Check if the next argument exists
+            if [[ $((i + 1)) -le $# ]]; then
+                project_type="${(P)$((i + 1))}"
+                ((i++))  # Skip the next argument since it's the value for --branch
+            else
+                echo "Error: --type requires a value."
+                exit 1
+            fi
+            ;;
         --branch)
             # Check if the next argument exists
             if [[ $((i + 1)) -le $# ]]; then
@@ -66,6 +89,9 @@ for (( i = 1; i <= $#; i++ )); do
         --copy-branch)
             copy_branch=true
             ;;
+        --skip-notion)
+            skip_notion=true
+            ;;
         --help)
             help=true
             ;;
@@ -80,13 +106,21 @@ if [ $help = true ]; then
     echo "Usage: create-project.zsh [options]"
     echo ""
     echo "Options:"
+    echo "  --name        Specify the project name (required)"
+    echo "  --type        Specify the project type (default: 'story')"
     echo "  --branch        Specify the branch name (required)"
     echo "  --ui            Create a worktree for the UI"
     echo "  --bff           Create a worktree for the BFF"
     echo "  --shell         Create a worktree for the Shell"
     echo "  --copy-branch   Copy the branch name to the clipboard"
+    echo "  --skip-notion   Skip creating a Notion project"
     echo "  --help          Display this help message"
     exit 0
+fi
+
+if [ -z $name ]; then
+    echo "Error: --name is required."
+    exit 1
 fi
 
 if [ -z $branch_name ]; then
@@ -110,6 +144,16 @@ fi
 # Create worktree for Shell
 if [ $ui = true ]; then
     create_worktree "di-shell" $branch_name
+fi
+
+if [ $skip_notion = false ]; then
+    # Extract JIRA ID from branch name
+    if [[ $branch_name =~ ^([A-Z]+-[0-9]+) ]]; then
+        jira_id=${match[1]}  # This captures the first group
+        echo "Jira ID" $jira_id
+    fi
+    output_heading "Creating Notion project for '$name'"
+    create-notion-project.zsh --name $name --jira-id $jira_id --branch $branch_name --type $project_type --current
 fi
 
 # Copy branch name to clipboard as it might be handy
