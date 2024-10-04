d4b810fa8cfaf21f796f6a4dc7e2cc6b60a5a54a
Greg Gannicott <greg.gannicott@precisely.com>
2024-10-04 17:32:17 +0100

    nvim - Update completion keybindingd to map Kickstart approach

--- nvim/.config/nvim/lua/core/plugin-config/completions.lua
+++ nvim/.config/nvim/lua/core/plugin-config/completions.lua
@@ -13,13 +13,29 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete({}),
-		["<CR>"] = cmp.mapping.confirm({
+		-- Use 'y' rather than 'enter' to confirm because.... Teej does.
+		-- I don't actually know the reasoining behind it though.
+		["<C-y>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
+		-- <c-l> will move you to the right of each of the expansion locations.
+		-- <c-h> is similar, except moving you backwards.
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
 	}),
 	sources = {
 		{ name = "nvim_lsp" },
