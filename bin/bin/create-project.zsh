5999edc46842295d2a334fee0b6ac1aac8a16975
Greg Gannicott <greg.gannicott@precisely.com>
2024-11-04 17:02:31 +0000

    Various - Extract install_dependencies out into helper-functions.zsh

--- bin/bin/create-project.zsh
+++ bin/bin/create-project.zsh
@@ -6,29 +6,6 @@ script_directory="$(cd "$(dirname "$0")" && pwd)"
 # Source the helper script
 source "$script_directory/helper-functions.zsh"
 
-install_dependencies ()
-{
-    id=$1
-    output_heading "Installing dependencies for $id"
-    if [ -e ~/.workflow-config.yaml ]; then
-        init_command=`yq ".repos[] | select(.id == \"$id\") | .init" ~/.workflow-config.yaml`
-        if [ "$init_command" = "" ]
-        then
-            echo "No init command found. Skipping..."
-        else
-            echo "Running init command: '$init_command'"
-            eval $init_command
-
-            if [ $? -ne 0 ]
-            then
-                echo "$(tput setaf 1)Error running init command...$(tput sgr0)"
-            fi
-        fi
-    else
-        echo "Not attepting to initialise repo. 'workflow-config.yaml' not found..."
-    fi
-}
-
 create_worktree ()
 {
     id=$1
--- bin/bin/helper-functions.zsh
+++ bin/bin/helper-functions.zsh
@@ -6,3 +6,25 @@ output_heading ()
     echo ""
 }
 
+install_dependencies ()
+{
+    id=$1
+    output_heading "Installing dependencies for $id"
+    if [ -e ~/.workflow-config.yaml ]; then
+        init_command=`yq ".repos[] | select(.id == \"$id\") | .init" ~/.workflow-config.yaml`
+        if [ "$init_command" = "" ]
+        then
+            echo "No init command found. Skipping..."
+        else
+            echo "Running init command: '$init_command'"
+            eval $init_command
+
+            if [ $? -ne 0 ]
+            then
+                echo "$(tput setaf 1)Error running init command...$(tput sgr0)"
+            fi
+        fi
+    else
+        echo "Not attepting to initialise repo. 'workflow-config.yaml' not found..."
+    fi
+}
