be7b5984d9a1e315c6b532fa42176f10d2e25302
Greg Gannicott <greg@gannicott.co.uk>
2025-02-08 17:06:34 +0000

    neovim - Remote harpooon telescope integration
    It has already been commented out for some time.

--- nvim/.config/nvim/lua/core/plugin-config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugin-config/telescope.lua
@@ -64,26 +64,6 @@ require("telescope").load_extension("undo")
 require("telescope").load_extension("noice")
 require("telescope").load_extension("live_grep_args")
 
--- Harpoon integration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
-end
-
 -- Define keymappings to display presets
 wk.add({
 	{
