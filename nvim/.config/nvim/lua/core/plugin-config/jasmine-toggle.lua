b77dd389b458e1f5f0a24b105c1cf2a42634dabe
Greg Gannicott <greg.gannicott@precisely.com>
2025-03-19 10:46:01 +0000

    nvim: add it/fit toggle for jasmine tests
    Add a new keybinding (<leader>ti) that toggles between 'it' and 'fit'
    tests in Jasmine test files. This complements the existing describe/fdescribe
    toggle functionality, making it easier to focus on specific test cases
    during development.

--- nvim/.config/nvim/lua/core/plugin-config/jasmine-toggle.lua
+++ nvim/.config/nvim/lua/core/plugin-config/jasmine-toggle.lua
@@ -3,4 +3,5 @@ local wk = require("which-key")
 wk.add({
 	{ "<leader>t", group = "Testing" },
 	{ "<leader>td", require("jasmine-toggle").toggle_describe, desc = "Toggle describe/fdescribe" },
+	{ "<leader>ti", require("jasmine-toggle").toggle_it, desc = "Toggle it/fit" },
 })
