e47d768b1d686046b3a1b915e9ab1a0442120864
Greg Gannicott <greg.gannicott@precisely.com>
2025-03-19 14:09:17 +0000

    nvim: Add mappings for toggling xdescribe and xit

--- nvim/.config/nvim/lua/core/plugin-config/jasmine-toggle.lua
+++ nvim/.config/nvim/lua/core/plugin-config/jasmine-toggle.lua
@@ -4,4 +4,6 @@ wk.add({
 	{ "<leader>t", group = "Testing" },
 	{ "<leader>td", require("jasmine-toggle").toggle_describe_focus, desc = "Toggle describe/fdescribe" },
 	{ "<leader>ti", require("jasmine-toggle").toggle_it_focus, desc = "Toggle it/fit" },
+	{ "<leader>tD", require("jasmine-toggle").toggle_describe_skip, desc = "Toggle describe/xdescribe" },
+	{ "<leader>tI", require("jasmine-toggle").toggle_it_skip, desc = "Toggle it/xit" },
 })
