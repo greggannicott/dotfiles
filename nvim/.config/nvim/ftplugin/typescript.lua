85788cf31254462c0dfceda15ffac5bc41c9d907
Greg Gannicott <greg.gannicott@precisely.com>
2025-03-27 15:09:37 +0000

    nvim: Disable add_async helper
    You've been unable to get it to work in all scenarios.
    
    Note that this commit does contain some changes to help though.

--- nvim/.config/nvim/ftplugin/typescript.lua
+++ nvim/.config/nvim/ftplugin/typescript.lua
@@ -11,17 +11,23 @@ local function find_ancestor(node, types)
 end
 
 local function add_async()
-	-- Insert a 't' as us capturing the t press means it doesn't happen naturally.
-	vim.api.nvim_feedkeys("t", "n", true)
-
-	-- Find out whether the preceding word is `awai`.
 	local cursor_position = vim.api.nvim_win_get_cursor(0)
 	local current_row = cursor_position[1]
 	local current_col = cursor_position[2]
+
+	-- Insert the 't' character to replace the 't' which just acted on.
+	-- Instead of feeding keys, directly insert 't' and update cursor position.
+	-- This is to make it work normal mode via the command line and macros.
+	vim.api.nvim_buf_set_text(0, current_row - 1, current_col, current_row - 1, current_col, { "t" })
+
+	-- Move cursor after the inserted 't'
+	vim.api.nvim_win_set_cursor(0, { current_row, current_col + 1 })
+
 	-- If the cursor is at the start of the line, we can't insert an `async` keyword.
 	if current_col < 4 then
 		return
 	end
+
 	local preceding_text =
 		vim.api.nvim_buf_get_text(0, current_row - 1, current_col - 4, current_row - 1, current_col, {})[1]
 	if preceding_text ~= "awai" then
@@ -48,7 +54,11 @@ local function add_async()
 end
 
 -- Trigger `add_async` when user presses "t". This will insert an `async` if the function requires due to the use of an `await`.
-vim.keymap.set("i", "t", add_async, { buffer = true })
+-- This has been disabled as you couldn't get it to work in all of the following scenarions. Only some:
+-- - When normal mode is entered via the command line.
+-- - When used as part of a macros called via command line.
+-- - When used as part of a '.' repeat command.
+-- vim.keymap.set("i", "t", add_async, { buffer = true })
 
 -- Used when you wish to debug. Introduce it and then whenever you want to reload this file you can use the keymapping specified.
---vim.keymap.set("n", "<leader>rr", "<cmd>so ~/.config/nvim/ftplugin/typescript.lua<cr>")
+vim.keymap.set("n", "<leader>rr", "<cmd>so ~/.config/nvim/ftplugin/typescript.lua<cr>")
