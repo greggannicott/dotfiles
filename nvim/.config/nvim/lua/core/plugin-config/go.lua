ee68f6473d3f0749b19ef474dec5f51b2175dc0a
Greg Gannicott <greg.gannicott@precisely.com>
2025-06-19 16:40:51 +0100

    nvim: Add golang related key bindings

--- nvim/.config/nvim/lua/core/plugin-config/go.lua
+++ nvim/.config/nvim/lua/core/plugin-config/go.lua
@@ -1,1 +1,12 @@
 require("go").setup()
+local wk = require("which-key")
+
+wk.add({
+	{ "<leader>cg", group = "Go" },
+	{ "<leader>cga", ":GoAlt<CR>", desc = "Toggle Between Alt Files" },
+	{ "<leader>cgd", ":GoDef<CR>", desc = "Go to Definition" },
+	{ "<leader>cgr", ":GoRename<CR>", desc = "Rename" },
+	{ "<leader>cgs", ":GoSignature<CR>", desc = "Signature Help" },
+	{ "<leader>cgt", ":GoTestFunc<CR>", desc = "Test Function" },
+	{ "<leader>cgp", ":GoPkgBrowser<CR>", desc = "Package Browser" },
+}, { mode = "n" })
