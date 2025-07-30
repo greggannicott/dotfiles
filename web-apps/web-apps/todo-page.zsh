65191cefb7f82484d2fccb512d028d1afe217522
Greg Gannicott <greg.gannicott@precisely.com>
2025-07-30 15:50:18 +0100

    misc: Add web app for new task management page

---
+++ web-apps/web-apps/todo-page.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+  --app="https://www.notion.so/gregggannicott/Work-Tasks-Overview-Board-1f60b997e3124dcda12765b85ac8c30f" \
+  --user-data-dir="/Users/greggannicott/ChromeProfiles/todo-page" 
+
