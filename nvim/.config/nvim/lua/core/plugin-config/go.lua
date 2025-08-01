cbc186bdd9382a08106b05f755dfd64bd9270853
Greg Gannicott <greg.gannicott@precisely.com>
2025-08-01 12:50:53 +0100

    nvim: Update keybindings for golang

--- nvim/.config/nvim/lua/core/plugin-config/go.lua
+++ nvim/.config/nvim/lua/core/plugin-config/go.lua
@@ -3,10 +3,12 @@ local wk = require("which-key")
 
 wk.add({
 	{ "<leader>cg", group = "Go" },
-	{ "<leader>cga", ":GoAlt<CR>", desc = "Toggle Between Alt Files" },
+	{ "<leader>cga", ":GoAlt!<CR>", desc = "Toggle Between Alt Files" },
+	{ "<leader>cgA", ":GoAltV!<CR>", desc = "Toggle Between Alt Files" },
 	{ "<leader>cgd", ":GoDef<CR>", desc = "Go to Definition" },
 	{ "<leader>cgr", ":GoRename<CR>", desc = "Rename" },
 	{ "<leader>cgs", ":GoSignature<CR>", desc = "Signature Help" },
 	{ "<leader>cgt", ":GoTestFunc<CR>", desc = "Test Function" },
+	{ "<leader>cgT", ":GoTestFile<CR>", desc = "Test File" },
 	{ "<leader>cgp", ":GoPkgBrowser<CR>", desc = "Package Browser" },
 }, { mode = "n" })
