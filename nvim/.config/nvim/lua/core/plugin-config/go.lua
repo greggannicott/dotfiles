17c1e81c6f21a8246e8adbbb3a70f2ba7e6dcdd2
Greg Gannicott <greg.gannicott@precisely.com>
2025-08-08 12:16:31 +0100

    nvim: Improve description of "Toggle Between Alt Files" for vertical option

--- nvim/.config/nvim/lua/core/plugin-config/go.lua
+++ nvim/.config/nvim/lua/core/plugin-config/go.lua
@@ -5,7 +5,7 @@ local wk = require("which-key")
 wk.add({
 	{ "<leader>cg", group = "Go" },
 	{ "<leader>cga", ":GoAlt!<CR>", desc = "Toggle Between Alt Files" },
-	{ "<leader>cgA", ":GoAltV!<CR>", desc = "Toggle Between Alt Files" },
+	{ "<leader>cgA", ":GoAltV!<CR>", desc = "Toggle Between Alt Files (Vertical)" },
 	{ "<leader>cgd", ":GoDef<CR>", desc = "Go to Definition" },
 	{ "<leader>cgr", ":GoRename<CR>", desc = "Rename" },
 	{ "<leader>cgs", ":GoSignature<CR>", desc = "Signature Help" },
