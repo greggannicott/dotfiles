3250c587b6826664dc9f1a6a48cfa2d1fce4266d
Greg Gannicott <greg.gannicott@precisely.com>
2025-10-01 11:13:49 +0100

    obsidian: Only list notes from `/notes/` directory

--- bin/bin/open-obsidian-file-in-split.zsh
+++ bin/bin/open-obsidian-file-in-split.zsh
@@ -2,4 +2,4 @@
 
 # Displays a list of all markdown files in the Obsidian vault and opens the selected file in a new tmux pane using glow.
 cd "`yq \".obsidian-directory\" ~/.workflow-config.yaml`"
-find . -name "*.md" | fzf-tmux -p --border --cycle --reverse --header="Select Obsidian File" --info="inline-right" | xargs -I {} tmux split-window -h -f glow -p "{}"
+find ./notes/ -name "*.md" | fzf-tmux -p --border --cycle --reverse --header="Select Obsidian File" --info="inline-right" | xargs -I {} tmux split-window -h -f glow -p "{}"
