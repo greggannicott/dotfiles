fc9f17a3dc702ee6c47bea8f62278d246917d616
Greg Gannicott <greg.gannicott@precisely.com>
2025-10-16 11:43:48 +0100

    nvim: Add snippet for adding a trace log entry in Go

---
+++ nvim/.config/nvim/lua/core/snippets/go.lua
@@ -0,0 +1,8 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+	s({ trig = "tl", name = "Tracer Log" }, fmt('logging.Tracer.AtTraceLevel(logsettings.Verbose).Log("{}");', i(1))),
+}
