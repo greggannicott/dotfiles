0557fd4c7d485db05884bbd9d913d13d380cd56a
Greg Gannicott <greg.gannicott@precisely.com>
2025-11-07 21:06:34 +0000

    nvim: Improve tracer log snipped.
    Add additional placeholder location for when you want to include
    variables.

--- nvim/.config/nvim/lua/core/snippets/go.lua
+++ nvim/.config/nvim/lua/core/snippets/go.lua
@@ -4,5 +4,8 @@ local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-	s({ trig = "tl", name = "Tracer Log" }, fmt('logging.Tracer.AtTraceLevel(logsettings.Verbose).Log("{}");', i(1))),
+	s(
+		{ trig = "tl", name = "Tracer Log" },
+		fmt('logging.Tracer.AtTraceLevel(logsettings.Verbose).Log("{}"{});', { i(1), i(2) })
+	),
 }
