1b37c0b501588c2ab3c1e9ce56cc7e5d3e5b8e80
Greg Gannicott <greg.gannicott@precisely.com>
2026-01-08 11:52:19 +0000

    open-jira: Various
    - cp rather than c is now used to specify current project.
    - when cb is specified, the contents of the clipboard is now used.

--- bin/bin/open-jira.zsh
+++ bin/bin/open-jira.zsh
@@ -2,23 +2,30 @@
 
 usage="Usage: open-jira <ticket_id|c>
 
-c = open ticket for current project"
+cp = open ticket for current project
+cb = use contents of clipboard"
 
 open_jira () {
     local id=$1
 
+    # If no value is provided, use the contents of the clipboard
     if [[ -z "$id" ]]; then
         echo $usage 
         return 1
     fi
 
-    if [[ $id = "c" ]]; then
+    if [[ $id = "cp" ]]; then
         id=$(get_current_project_jira_id)
         if [[ -z "$id" ]]; then
             echo "Unable to open ticket. No Jira ID found for current project."
             return 1
         fi
     fi
+
+    if [[ $id = "cb" ]]; then
+        id=$(pbpaste)
+    fi
+
     open "https://jira.syncsort.com/browse/${id}"
 }
 
