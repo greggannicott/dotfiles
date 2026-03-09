dd9e40f0fc5205f0c640015fe1c551a1d09dae84
Greg Gannicott <greg.gannicott@precisely.com>
2026-03-09 15:55:04 +0000

    nvim: Remove unused null-ls formatters
    Removing so warnings are no longer displayed on launch of nvim

--- nvim/.config/nvim/lua/core/plugin-config/lsp.lua
+++ nvim/.config/nvim/lua/core/plugin-config/lsp.lua
@@ -183,8 +183,6 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.beautysh,
-		null_ls.builtins.formatting.fixjson,
 	},
 })
 
