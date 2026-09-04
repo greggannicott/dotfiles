0e7fc1104866e9e1fdb6830f136b7a15d803a920
Greg Gannicott <greg.gannicott@precisely.com>
2026-09-04 12:12:57 +0100

    open-linear-id: use contents of clipboard when "cb" is passed in

--- bin/bin/open-linear-id.zsh
+++ bin/bin/open-linear-id.zsh
@@ -3,10 +3,21 @@
 script_directory="$(cd "$(dirname "$0")" && pwd)"
 source "$script_directory/helper-functions.zsh"
 
-usage="Usage: open-linear-id <linear_id>"
+usage="Usage: open-linear-id [linear_id|cb]
+
+cb = use contents of clipboard"
 
 open_linear () {
     local id=$1
+
+    if [[ -z "$id" ]]; then
+        id=$(gum input --header="Linear ID:")
+        check_exit_code $?
+    fi
+
+    if [[ $id = "cb" ]]; then
+        id=$(pbpaste)
+    fi
 
     if [[ -z "$id" ]]; then
         id=$(gum input --header="Linear ID:")
